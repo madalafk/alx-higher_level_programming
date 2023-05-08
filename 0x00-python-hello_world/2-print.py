32a02354ccb1e350f7710bf123d9e87609a53644
fmadala@hotmail.com <fmadala@hotmail.com>
2023-05-08 10:42:32 -0700

    Hello, print

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 # this will print the string below followed by a newline
-print("Programming is like building a multilingual puzzle,\n")
+print("\"Programming is like building a multilingual puzzle,\n")
