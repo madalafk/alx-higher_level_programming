04c0acd923d8bca21a1d382483d951016ba0a077
fmadala@hotmail.com <fmadala@hotmail.com>
2023-05-09 14:31:33 -0700

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,5 +2,7 @@
 
 # prints the ASCII alphabet, in reverse code
 
-for c in range(ord('Z'), ord('A') - 1, -1):
-    print("{}{}".format(chr(c + 32), chr(c)), end="")
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
