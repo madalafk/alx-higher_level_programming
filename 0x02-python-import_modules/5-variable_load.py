7f4d96a5b56e220702149460e68f6539804e0f5e
fmadala@hotmail.com <fmadala@hotmail.com>
2023-05-11 23:31:20 -0700

    Everything can be imported

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from variable_load_5 import a
+
+'''
+A program that imports the variable 'a' from the file variable_load_5.py
+and prints its value.
+'''
+
+def find_var():
+    print(a)
+
+
+if __name__ == "__main__":
+    find_var()
