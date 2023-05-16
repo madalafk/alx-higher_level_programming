00971a8e04b5ffd78cb5303bb5213633b800938b
fmadala@hotmail.com <fmadala@hotmail.com>
2023-05-16 12:31:41 -0700

    Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+
+    if (idx < 0) or (idx > (len(my_list)-1)):
+        return my_list
+
+    copy_of_original = [i for i in my_list]
+    copy_of_original[idx] = element
+    return copy_of_original
