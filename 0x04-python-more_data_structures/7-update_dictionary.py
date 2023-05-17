6b058e32de973114181666a292907f4bbf011370
fmadala@hotmail.com <fmadala@hotmail.com>
2023-05-17 12:50:44 -0700

    Update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    This function Replaces or adds key/value pairs in a dictionary.
+    """
+    a_dictionary[key] = value
+    return a_dictionary
+
