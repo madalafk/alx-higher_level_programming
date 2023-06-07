69f92b97af58210e1fc091da6066eb9dc3c57334
fmadala@hotmail.com <fmadala@hotmail.com>
2023-06-07 12:53:58 -0700

    30. Low memory cost

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Classclass LockedClass with no class or object attribute
+    prevents the user from dynamically creating new instance attributes
+    except if the new instance attribute is called first_name
+"""
+
+class LockedClass:
+    __slots__ = ['first_name']
