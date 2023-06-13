6acaad45ed03bc0ce3015ee788f8249c83532ef0
fmadala@hotmail.com <fmadala@hotmail.com>
2023-06-13 13:06:20 -0700

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-# 11-student.py
+# 9-student.py
 
 """This module contains  a class Student that defines a student.
 """
 
 
 class Student:
-    """Represent a class (student)."""
+    """ Class to create student instances """
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-        Args:
-            first_name (str): First name of the student.
-            last_name (str): Last name of the student.
-            age (int): Age of the student.
-        """
+        """ Special method to initialize """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Get a dict representation of the Student."""
-        return self.
+        """ Method that returns directory description """
+        return (self.__dict__.copy())
