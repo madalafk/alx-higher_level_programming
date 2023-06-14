d92d56589d15bb14df9a632965013d9962ad892c
fmadala@hotmail.com <fmadala@hotmail.com>
2023-06-14 10:55:32 -0700

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 # 1-my_list.py
+
 """This function defines an inherited list class 'MyList'."""
 
 
 class MyList(list):
-    """Inherits from the built-in 'lis' class."""
+    """Inherits from the built-in 'list' class."""
+
+    def __init__(self):
+        super().__init__()
 
     def print_sorted(self):
         """Print a list in sorted ascending order."""
