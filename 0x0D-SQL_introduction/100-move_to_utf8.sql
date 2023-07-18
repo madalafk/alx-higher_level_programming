c43541a32bc945c71cb301b6d97cc955721a665a
fmadala@hotmail.com <fmadala@hotmail.com>
2023-07-18 15:15:25 -0700

    100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,3 @@
 -- This converts hbtn_0c_0 DB to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
-USE `hbtn_0c_0`;
-ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.second_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
