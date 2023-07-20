28f8776eeb882ec0e558154f8ecb55a4d4dc1581
fmadala@hotmail.com <fmadala@hotmail.com>
2023-07-20 09:42:39 -0700

    9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,7 @@
 -- This lists all cities contained in the database hbtn_0d_usa
 
 SELECT c.id, c.name, s.name
-FROM cities c, states s
-WHERE c.state_id = s.id
-ORDER BY c.id;
+  FROM cities AS c
+       INNER JOIN states AS s
+       ON c.state_id = s.id
+ ORDER BY c.id;
