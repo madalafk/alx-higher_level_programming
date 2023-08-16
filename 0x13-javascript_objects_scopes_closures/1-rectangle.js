71de76c15e80bda432585172d10147cf4a5265ad
fmadala@hotmail.com <fmadala@hotmail.com>
2023-08-16 11:34:08 -0700

    1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/**
+ * class rectangle with width and height attributes
+ */
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
