0cdbee5613a47fb02423b00dd73a8be8e30a10c8
fmadala@hotmail.com <fmadala@hotmail.com>
2023-08-17 09:20:09 -0700

    100-map.js

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/**
+ * this script imports an array and computes a new array
+ */
+
+const list = require('./100-data.js').list;
+
+const newList = list.map((val, idx) => val * idx);
+console.log(list);
+console.log(newList);
