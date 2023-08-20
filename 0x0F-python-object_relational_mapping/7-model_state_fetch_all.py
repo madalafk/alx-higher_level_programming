5d15f4c28505970cded05e484cf920dec9b0cc09
fmadala@hotmail.com <fmadala@hotmail.com>
2023-08-20 01:37:31 -0700

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-""" listing all State objects from the database hbtn_0e_6_usa
-using SQLAlchemy module
+""" Listing all State objects from the database hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
+    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    mysql = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database_name)
+    engine = create_engine(mysql)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).order_by(State.id):
-        print(instance.id, instance.name, sep=": ")
+
+    for item in session.query(State).order_by(State.id):
+        print("{:d}: {:s}".format(item.id, item.name))
+
+    session.close()
+
+if __name__ == "__main__":
+    main()
