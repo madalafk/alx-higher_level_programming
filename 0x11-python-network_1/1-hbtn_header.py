d102230bbf1a74375c4b97bedb44b56c928bd263
madalafk <fmadala@hotmail.com>
2023-09-03 03:07:04 -0700

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 """
-this  takes in a URL, sends a request to the URL and displays the value 
- of the X-Request-Id variable found in the header of the response.
+this  takes in a URL, sends a request to the URL and displays the value
+-of the X-Request-Id variable found in the header of the response.
 """
 
 import urllib.request
