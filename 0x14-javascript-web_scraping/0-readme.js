3da15f9d9dbcbd377d3334658ca070267e65b6dd
fmadala@hotmail.com <fmadala@hotmail.com>
2023-09-26 11:58:31 -0700

    0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// reads and prints the content of a file.
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', function (error, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
