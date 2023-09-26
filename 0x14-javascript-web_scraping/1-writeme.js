0ee5b7f2bddcc54f8c29d7afc2993da2e593f062
fmadala@hotmail.com <fmadala@hotmail.com>
2023-09-26 12:14:10 -0700

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// writes a string to a file.
+const fs = require('fs');
+const file = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(file, string, 'utf-8', function (error) {
+  if (error) console.log(err);
+});
